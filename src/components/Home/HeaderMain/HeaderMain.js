bb246e96c0080910052bf68d3b9c7ddd469dae94
shahjalalcse <shah-jalal1>
2021-04-10 15:33:12 +0600

    doctors portal homepage done

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../../images/Chair.png'
+
+const HeaderMain = () => {
+    return (
+        <main style={{height:'600px'}} className="row d-flex align-items-center">
+            <div className="col-md-4 offset-md-1">
+                <h1 style={{color: '#3A4256'}}>Your New Smile <br/> Starts Here</h1>
+                <p className="text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci modi nobis similique molestias ratione dolor?</p>
+                <button className="btn btn-primary">GET APPOITMENT</button>
+            </div>
+            <div className="col-md-6">
+                <img src={chair} alt="" className="img-fluid"/>
+            </div>
+        </main>
+    );
+};
+
+export default HeaderMain;
