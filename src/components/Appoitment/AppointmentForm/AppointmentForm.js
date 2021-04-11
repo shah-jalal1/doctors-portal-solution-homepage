55953d673c114e4750c3c15907e5b4ed56889764
shahjalalcse <shah-jalal1>
2021-04-12 00:48:51 +0600

    form problem solved

--- src/components/Appoitment/AppointmentForm/AppointmentForm.js
+++ src/components/Appoitment/AppointmentForm/AppointmentForm.js
@@ -37,15 +37,30 @@ const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn }) => {
 
 
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register('firstName')} /> {/* register an input */}
-                    <input {...register('lastName', { required: true })} />
+
+                    <div className="form-group">
+                   <input type="text" {...register('name') }  name="name" placeholder="Your Name" className="form-control" />
+                        {errors.name && <span className="text-danger">This field is required</span>} 
+                       <input {...register('firstName') } className="form-control" /> 
+                        {errors.name && <p className="text-danger">Please enter number for age.</p>}
+                        {errors.name && <span className="text-danger">This field is required</span>}
+                        {errors.email && <span className="text-danger">This field is required</span>}
+                        <input type="text" {...register('email') } name="email" placeholder="Email" className="form-control" />
+
+                    </div>
+                    <div className="form-group text-right">
+                        <button type="submit" className="btn btn-brand">Send</button>
+                    </div>
+
+                    {/* <input {...register('firstName')} /> {/* register an input */}
+                    {/* <input {...register('lastName', { required: true })} />
                     {errors.lastName && <p>Last name is required.</p>}
                     <input {...register('age', { pattern: /\d+/ })} />
                     {errors.age && <p>Please enter number for age.</p>}
-                    <input type="submit" />
+                    <input type="submit" /> */}
                 </form>
 
-                
+
 
             </Modal>
         </div>
