73d71b2c592c512c0c168d22430dd2247df13b52
shahjalalcse <shah-jalal1>
2021-04-12 00:53:39 +0600

    form full done

--- src/components/Appoitment/AppointmentForm/AppointmentForm.js
+++ src/components/Appoitment/AppointmentForm/AppointmentForm.js
@@ -39,25 +39,46 @@ const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn }) => {
                 <form onSubmit={handleSubmit(onSubmit)}>
 
                     <div className="form-group">
-                   <input type="text" {...register('name') }  name="name" placeholder="Your Name" className="form-control" />
-                        {errors.name && <span className="text-danger">This field is required</span>} 
-                       <input {...register('firstName') } className="form-control" /> 
-                        {errors.name && <p className="text-danger">Please enter number for age.</p>}
+                        <input type="text" {...register('firstName')} name="name" placeholder="Your Name" className="form-control" />
                         {errors.name && <span className="text-danger">This field is required</span>}
-                        {errors.email && <span className="text-danger">This field is required</span>}
-                        <input type="text" {...register('email') } name="email" placeholder="Email" className="form-control" />
 
                     </div>
+                    <div className="form-group">
+                        <input type="text"{...register('firstName')} name="phone" placeholder="Phone Number" className="form-control" />
+                        {errors.phone && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        <input type="text" {...register('firstName')} name="email" placeholder="Email" className="form-control" />
+                        {errors.email && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-4">
+
+                            <select className="form-control" name="gender" {...register('firstName')} >
+                                <option disabled={true} value="Not set">Select Gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Not set">Other</option>
+                            </select>
+                            {errors.gender && <span className="text-danger">This field is required</span>}
+
+                        </div>
+                        <div className="col-4">
+                            <input {...register('firstName')} className="form-control" name="age" placeholder="Your Age" type="number" />
+                            {errors.age && <span className="text-danger">This field is required</span>}
+                        </div>
+                        <div className="col-4">
+                            <input {...register('firstName')} className="form-control" name="weight" placeholder="Weight" type="number" />
+                            {errors.weight && <span className="text-danger">This field is required</span>}
+                        </div>
+                    </div>
+
                     <div className="form-group text-right">
                         <button type="submit" className="btn btn-brand">Send</button>
                     </div>
 
                     {/* <input {...register('firstName')} /> {/* register an input */}
-                    {/* <input {...register('lastName', { required: true })} />
-                    {errors.lastName && <p>Last name is required.</p>}
-                    <input {...register('age', { pattern: /\d+/ })} />
-                    {errors.age && <p>Please enter number for age.</p>}
-                    <input type="submit" /> */}
+
                 </form>
 
 
@@ -68,3 +89,5 @@ const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn }) => {
 };
 
 export default AppointmentForm;
+
+
