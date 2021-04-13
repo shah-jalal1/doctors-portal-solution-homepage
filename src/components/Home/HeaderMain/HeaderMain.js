6fd165a7810801367228262805fb424b4edb9f57
shahjalalcse <shah-jalal1>
2021-04-13 13:48:51 +0600

    link added in get appointment

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import chair from '../../../images/chair.png'
+import {
+    Link
+  } from "react-router-dom";
 
 const HeaderMain = () => {
     return (
-        <main style={{height:'600px'}} className="row d-flex align-items-center">
+        <main style={{ height: '600px' }} className="row d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
-                <h1 style={{color: '#3A4256'}}>Your New Smile <br/> Starts Here</h1>
+                <h1 style={{ color: '#3A4256' }}>Your New Smile <br /> Starts Here</h1>
                 <p className="text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci modi nobis similique molestias ratione dolor?</p>
-                <button className="btn btn-primary">GET APPOITMENT</button>
+                {/* <button className="btn btn-primary">GET APPOITMENT</button> */}
+                
+                <Link to="/appointment">
+                    <button className="btn btn-primary">GET APPOITMENT</button>
+                </Link>
             </div>
             <div className="col-md-6">
-                <img src={chair} alt="" className="img-fluid"/>
+                <img src={chair} alt="" className="img-fluid" />
             </div>
         </main>
     );
