9d6c1737768d60b2cf3a4fd6952d861927947d31
shahjalalcse <shah-jalal1>
2021-04-13 12:20:57 +0600

    appointment short list

--- src/components/Dashboard/AppointmentByDate/AppointmentByDate.js
+++ src/components/Dashboard/AppointmentByDate/AppointmentByDate.js
@@ -1,12 +1,18 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
+import AppointmentShortList from '../../Appoitment/AppointmentShortList/AppointmentShortList';
 
 const AppointmentByDate = ({appointments}) => {
     return (
         <div>
-            <h1>Appoitment: {appointments.length}</h1>
+            <h2 className="text-brand text-center">Appointments</h2>
             {
-                appointments.map(app => <li key={app._id}>{app.name}</li>)
+                appointments.length ?
+                 <AppointmentShortList appointments={appointments}></AppointmentShortList>
+                :
+                <div className="p-5">
+                    <h4 className="lead text-center">No Appointments for this Date</h4>
+                </div>
             }
             
         </div>
