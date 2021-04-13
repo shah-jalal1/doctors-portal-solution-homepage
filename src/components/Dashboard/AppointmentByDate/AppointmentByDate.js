6cacce50b0c9ddb8a1270dcc03f81e4790f58191
shahjalalcse <shah-jalal1>
2021-04-13 12:12:20 +0600

    sidebar icon solved

--- src/components/Dashboard/AppointmentByDate/AppointmentByDate.js
+++ src/components/Dashboard/AppointmentByDate/AppointmentByDate.js
@@ -1,9 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
 
 const AppointmentByDate = ({appointments}) => {
     return (
         <div>
             <h1>Appoitment: {appointments.length}</h1>
+            {
+                appointments.map(app => <li key={app._id}>{app.name}</li>)
+            }
             
         </div>
     );
